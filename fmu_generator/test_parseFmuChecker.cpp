0589251daed7bc1ef47cd17bd34ab00601cb8fea
Dario Mangoni <dariomangoni@gmail.com>
2023-09-21 14:44:37 +0200

    Better fmuChecker parser

--- fmu_generator/test_parseFmuChecker.cpp
+++ fmu_generator/test_parseFmuChecker.cpp
@@ -4,28 +4,38 @@
 #include <string>
 
 int main() {
+    std::cout << "Opening fmuChecker log: " << FMUCHECKER_LOG << std::endl;
+    std::cout << "-> status: ";
     std::ifstream file(FMUCHECKER_LOG);
     if (!file.is_open()) {
-        std::cerr << "Error opening file." << std::endl;
+        std::cerr << "ERROR" << std::endl;
         return 1;
     }
+    std::cout << " SUCCESS" << std::endl;
 
     std::regex errorRegex(R"(\[ERROR\](.*))");
     std::regex warningRegex(R"(\[WARNING\](.*))");
     std::string line;
 
-    bool hasError = false;
+    std::cout << "Parsing fmuChecker log..." << std::endl;
+    unsigned int errorCount = 0;
+    unsigned int warningCount = 0;
     while (std::getline(file, line)) {
         std::smatch match;
         if (std::regex_search(line, match, errorRegex)) {
             std::cout << "ERROR: " << match[1] << std::endl;
-            hasError = true;
+            errorCount++;
         }
         else if (std::regex_search(line, match, warningRegex)) {
             std::cout << "WARNING: " << match[1] << std::endl;
+            warningCount++;
         }
     }
 
     file.close();
-    return hasError ? 1 : 0;
+    std::cout << "Parsing completed with:" << std::endl;
+    std::cout << "- errors: " << errorCount << std::endl;
+    std::cout << "- warnings: " << warningCount << std::endl;
+
+    return errorCount>0;
 }
